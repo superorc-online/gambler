29a5a66ea84b6b2d76fc480e6041700a468de0cf
Thomas Smith <44484478+TSmith09@users.noreply.github.com>
2019-01-30 22:47:12 -0500

    Implemented functions in gambler.go

--- cmd/gambler/main.go
+++ cmd/gambler/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	
 	"github.com/superorc-online/gambler"
 )
 
@@ -10,7 +10,6 @@ func main() {
 	gs := &gambler.GamblingSession{}
 	gs.AddToBalance(5)
 	fmt.Printf("Welcome to The Random Number Draw! To win you have to draw an %d or higher. Each try cost $1.00. Your current balance is $%d.00. Would you like to play? (Yes/No) ", gambler.WinThreshold, gs.Balance())
-
 	//todo implement mainloop
 
 	fmt.Println("Goodbye.")
--- gambler.go
+++ gambler.go
@@ -1,48 +1,85 @@
 package gambler
 
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+// WinThreshold sets the number you have to match or beat to win
 const WinThreshold = 80
 
-type GamblingSession struct {
+// GamblingSession todo comment
+type GamblingSession struct { 
 	attempts int
 	wins int
 	losses int
 	balance int
 }
 
+// Attempts returns the number of attempts left in the current session
 func (gs *GamblingSession) Attempts() int {
 	return gs.attempts
 }
 
+// Wins returns the number of wins in the current session
 func (gs *GamblingSession) Wins() int {
 	return gs.wins
 }
 
+// Losses returns the number of losses in the current session
 func (gs *GamblingSession) Losses() int {
 	return gs.losses
 }
 
+// Balance returns the remaining balance in the current session
 func (gs *GamblingSession) Balance() int {
 	return gs.balance
 }
 
+// BuyAttempts reduces the balance and adds attempts to the current session
 func (gs *GamblingSession) BuyAttempts(quantity int) error {
-	//todo implement BuyAttempts
+	//todo implement BuyAttempts and validate input
+	if quantity > 0 {
+		gs.balance -= quantity
+		gs.attempts += quantity
+	} else {
+		return fmt.Errorf("%d is not a positive integer, please enter a positive integer", quantity)
+	}
 	return nil
 }
 
+// MakeAnAttempt generates a random number and checks it against the WinThreshold
 func (gs *GamblingSession) MakeAnAttempt() (bool, int, error) {
 	//todo implement MakeAnAttempt
-	return false, 0, nil
+	var result bool 
+	rand.Seed(time.Now().UnixNano())
+	roll := rand.Intn(100)
+	if roll >= WinThreshold {
+		result = true
+		gs.wins ++
+	} else {
+		result = false
+		gs.losses ++
+	}
+	return result, roll, nil
 }
 
+// AddToBalance adds money to the balance of the current session
 func (gs *GamblingSession) AddToBalance(dollars int) error {
 	//todo validate input
-	gs.balance += dollars
+	if dollars > 0 {
+		gs.balance += dollars
+	} else {
+		return fmt.Errorf("%d is not a positive integer, please enter a positive integer", dollars)
+	}
 	return nil
 }
 
+// CashOut refunds attempts left in the current session
 func (gs *GamblingSession) CashOut() error {
 	//todo implement CashOut
+	gs.balance += gs.attempts
 	return nil
 }
 
